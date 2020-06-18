9517940efa6ec8bae40c6faa2c7351fa60f8dbc6
enwony <enwony@gmail.com>
2020-06-19 02:52:51 +0300

    plugin frame

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :enwony_payed_issues_closing do
+  name 'Enwony PDF invoice plugin'
+  author 'Enwony enwony@gmail.com'
+  description 'Allow to generate pdf invoice for seleted issues in issue list view'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
